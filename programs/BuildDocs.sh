8062b8a72639ca1124c3b7f761f454893b477c54
Emil DeVries <EmilDeVries@hotmail.com>
2025-11-13 08:16:02 -0800

    changed builddoc file to stocklibprogs.txt

--- programs/BuildDocs.sh
+++ programs/BuildDocs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+filename=StockLibProgs.txt
+echo EUP5108 Stock Library and Program information > $filename
 # Find #$ comments at the beginning of a line and echos them without the #$
-./findinfiles.sh -r -n '^#\$' '*.asm' | sed 's/^#\$//' > StockPrograms.txt
+./findinfiles.sh -r -n '^#\$' '*.asm' | sed 's/^#\$//' >> $filename
 

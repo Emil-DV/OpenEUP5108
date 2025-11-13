8109440f2875a583aa969bb1e276d9ab0beec99b
Emil DeVries <EmilDeVries@hotmail.com>
2025-11-13 10:09:08 -0800

    Changed BuildDocs script to output StockLibProgs.md - added notes to script of expected md usage

--- programs/BuildDocs.sh
+++ programs/BuildDocs.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-filename=StockLibProgs.txt
-echo EUP5108 Stock Library and Program information > $filename
+filename=StockLibProgs.md
+# md code usage
+# use
+# #$**Warning** for important notes
+
+# #$---- at the top of each file
+# #$## {path and filename relative to programs folder} - {short description}
+
+# #$     {for each line of long description and consts and/or Vars }
+
+# #$
+# #$###  { for function names} - {short description}
+
+# Internal Global Consts & Vars should follow the pattern
+# {libAbrvConstName} or {libAbrv.VarName}
+
+# Internal labels and local vars should follow the pattern
+# {func.label} or {func.VarName}
+
+# The asssembler does not output label location with . in them
+
+echo \# EUP5108 Stock Library and Program information > $filename
 # Find #$ comments at the beginning of a line and echos them without the #$
 ./findinfiles.sh -r -n '^#\$' '*.asm' | sed 's/^#\$//' >> $filename
 

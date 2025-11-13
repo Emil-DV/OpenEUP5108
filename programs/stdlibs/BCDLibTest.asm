b54bbd812dc95cb9591d1293a537e9880843dd2e
Emil DeVries <EmilDeVries@hotmail.com>
2025-11-13 01:04:05 -0800

    added the bcd external test function

---
+++ programs/stdlibs/BCDLibTest.asm
@@ -0,0 +1,33 @@
+# Test function for the BCD code
+D BCDTestCmd
+S bcd\0
+
+D BCDtestHelp
+S bcd: Creates a BCD number and increments it as fast as possible\0
+
+# testBCD Creates a BCD number and increments it as fast as possible 
+D testBCD
+
+V testBCD.bcd		# The BCD variable
+a BCDLib.strlen
+
+  LDR testBCD.bcd
+  CAL BCDZro
+  
+  LDR VTCLR
+  CAL printStr1E
+
+D testBCD.gohere  	# do{
+
+  LDR VTHOME		# put the cursor at the top left
+  CAL printStr1E
+
+  LDR testBCD.bcd	# Print the BCD value
+#  CAL BCDPnt		#	as characters
+  CAL BCDPntLED 	# 	as BIGLEDs
+
+  LDR testBCD.bcd	# testBCD.bcd++;
+  CAL BCDInc
+  
+  JPL testBCD.gohere    # }while(1);
+#########################################################################

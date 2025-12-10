451dd95403785d7b85a1e4f1bffed65c83fb63ca
Emil DeVries <emildevries@hotmail.com>
2025-12-09 20:46:17 -0800

    created eupbnr.bat removed need for a buildnrun.bat in each folder

--- bin/eupbnr.bat
+++ bin/eupbnr.bat
@@ -1,13 +1,12 @@
 @echo off
 
-SET BaseName=EUPShell
-cd C:\Repos\OpenEUP5108\programs\shell
+SET BaseName=%1
+
 rem Assemble the file and if it succeeds run the emulator
-..\..\bin\eupasm.exe %BaseName%.asm 0x11  
+eupasm.exe %BaseName%.asm 0x11  
 IF %ERRORLEVEL% EQU 0 goto runit
 pause
 exit
 :runit
-pause
 rem Remove the 'R' at the end to bring up the emulator paused
-..\..\bin\eupemu.exe %BaseName%.rom 100000000 R
+eupemu.exe %BaseName%.rom 100000000 R
